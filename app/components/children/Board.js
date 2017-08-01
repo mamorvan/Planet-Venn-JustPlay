ce4330a5f402af089193b8f6e536128e599e0239
Mary Morvan <mamorvan@yahoo.com>
2017-08-01 14:42:41 -0400

    take away error messages from board :scissors:

--- app/components/children/Board.js
+++ app/components/children/Board.js
@@ -8,20 +8,20 @@ const Board = () => {
             <Row>
              
                 <div id="venn">                    
-                    <div id="category1" data-rule="red" alreadyPlaced="false"></div>
+                    <div id="category1" data-rule="red" ></div>
                     
-                    <div id="category3" data-rule="TBD" alreadyPlaced="false"></div>
+                    <div id="category3" data-rule="TBD" ></div>
         
-                    <div id="category2" data-rule="sun" alreadyPlaced="false"></div>           
+                    <div id="category2" data-rule="sun" ></div>           
                 </div>
    
             </Row>           
             <div>
                 <div>
                     
-                    <div id="category5" data-rule="TBD" alreadyPlaced="false"></div>
+                    <div id="category5" data-rule="TBD" ></div>
                     
-                    <img id="category4" data-rule="TBD" alreadyPlaced="false" src="./images/blackhole2.gif" />                 
+                    <img id="category4" data-rule="TBD" src="./images/blackhole2.gif" />                 
                 </div>     
             </div>
         </div>
